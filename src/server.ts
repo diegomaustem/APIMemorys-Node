26654cd5a40b59c312554c35b20d0bb5593fa837
maustem <diego.maustem@gmail.com>
2024-09-26 17:26:58 -0300

    :sparkles: feat: Setting cors API

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { usersRoutes } from './routes/users'
 
 const server = fastify()
 
+server.register(cors, {
+    origin: true
+})
 server.register(memoriesRoutes, usersRoutes)
 
 server.listen({
